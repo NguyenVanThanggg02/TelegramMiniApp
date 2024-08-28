09b6820c1025a67aaf594e68c0bfce6864a8e16c
kun_dev <cong.nguyen@vangxanh.net>
2024-08-28 13:56:49 +0700

    update

--- src/pages/admin/store/storeEdit.tsx
+++ src/pages/admin/store/storeEdit.tsx
@@ -195,7 +195,7 @@ const StoreEditPage: React.FC = () => {
                 // style={
                 //   !image.length ? { filter: "grayscale(1) opacity(0.5)" } : {}
                 // }
-                src={image.length > 0 ? image[0].src : DEFAULT_IMAGE_STORE}
+                src={image[0].src || DEFAULT_IMAGE_STORE}
                 alt="Store"
               />
               <Box className="upload-photo-icon">
