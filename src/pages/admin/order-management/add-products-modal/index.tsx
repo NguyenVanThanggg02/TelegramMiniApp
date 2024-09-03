3a5730399bc67578aad401fe98779c22c391be7b
kun_dev <cong.nguyen@vangxanh.net>
2024-09-03 07:42:19 +0700

    up

--- src/pages/admin/order-management/add-products-modal/index.tsx
+++ src/pages/admin/order-management/add-products-modal/index.tsx
@@ -11,17 +11,20 @@ interface ProductImage {
   uuid: string;
   url: string;
 }
+
 interface Product {
   uuid: string;
   name: string;
-  price: number;
+  price:number
   unit_price?: number;
   quantity?: number;
   images?: ProductImage[];
+  product_name: string;
   product_images?: ProductImage[];
   order_item_uuid: string
-  delivered_quantity: string
+  delivered_quantity: number
   product_uuid? : string
+  delivery_status: string
 }
 
 interface AddProductModalProps {
