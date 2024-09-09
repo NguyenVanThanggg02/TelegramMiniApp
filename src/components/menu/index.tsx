931d26f70a5b8756fb2d1b8d346cf2e6d01aff22
congnv <conganh28999@example.com>
2024-09-09 23:01:43 +0700

    fix

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -451,35 +451,37 @@ const MenuCommonPage: React.FC<MenuCommonPageProps> = () => {
               marginTop: table_uuid ? 100 : defaultMarginList,
             }}
           >
-            {Object.keys(displayProductList).map((cate,index) => {
-              menuRef.current[index] 
-             return (
-                <Box key={cate}>
-                  <Box
-                    flex
-                    justifyContent="space-between"
-                    mt={4}
-                    style={{ scrollMargin: "100px" }}
-                  >
-                    <Text size="xLarge" bold className="grey-color">
-                      {cate}
-                    </Text>
-                  </Box>
-  
-                  <DishMenu
-                    dishMenu={displayProductList[cate]}
-                    onDetails={(dish) => {
-                      setShowDishDetailsModal(true);
-                      handleSelectedDish(dish);
-                    }}
-                    onOrder={(dish) => {
-                      setShowOrderModal(true);
-                      handleSelectedDish(dish);
-                    }}
-                  />
+            {Object.keys(displayProductList).map((cate,index) => (
+              <Box key={cate}>
+                <Box
+                  flex
+                  justifyContent="space-between"
+                  mt={4}
+                    // @ts-ignore
+                  ref={(ref:any) => {
+                    menuRef.current[index] = ref!;
+                  }}
+                    // @ts-ignore
+                  style={{ scrollMargin: "100px" }}
+                >
+                  <Text size="xLarge" bold className="grey-color">
+                    {cate}
+                  </Text>
                 </Box>
-              )
-            })}
+
+                <DishMenu
+                  dishMenu={displayProductList[cate]}
+                  onDetails={(dish) => {
+                    setShowDishDetailsModal(true);
+                    handleSelectedDish(dish);
+                  }}
+                  onOrder={(dish) => {
+                    setShowOrderModal(true);
+                    handleSelectedDish(dish);
+                  }}
+                />
+              </Box>
+            ))}
           </Box>
 
           {!isEmpty(cart) && (
