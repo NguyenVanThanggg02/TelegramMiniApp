4ee8828eb77c5304094a2bd9bc5ac221bc3d46c3
kun_dev <cong.nguyen@vangxanh.net>
2024-09-10 17:14:31 +0700

    up

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -161,15 +161,20 @@ const MenuCommonPage: React.FC<MenuCommonPageProps> = () => {
     const handleScroll = () => {
       const container = pageRef.current;
       if (container) {
-        // const { scrollTop } = container;
-        // if (scrollTop === 0) return;
+        const { scrollTop, scrollHeight, clientHeight } = container;
+    
+        const isBottom = scrollTop + clientHeight >= scrollHeight;
+        
         menuRef.current.forEach((ref, index) => {
-          if (ref && ref.getBoundingClientRect().top <= 190) {
+          if (isBottom && index === menuRef.current.length - 1) {
+            setActiveTab(menu[index].uuid); 
+          } else if (ref && ref.getBoundingClientRect().top <= 210) {
             setActiveTab(menu[index].uuid);
           }
         });
       }
     };
+    
 
     const container = pageRef.current;
     container.addEventListener("touchmove", handleTouchMove);
