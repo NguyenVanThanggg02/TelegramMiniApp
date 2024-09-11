146da8eea8000e37d85502f02d202953c925d0fe
kun_dev <cong.nguyen@vangxanh.net>
2024-09-11 15:16:15 +0700

    up

--- src/pages/admin/table/index.tsx
+++ src/pages/admin/table/index.tsx
@@ -92,17 +92,17 @@ const TablePage: React.FC = () => {
     }
   };
 
-  const linkBuilder = (table_uuid: string): string => {
-    return `https://zalo.me/s//menu/${store_uuid}/${table_uuid}?tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
-  };
-  // quét vào thẳng store nhưng không quét để sang menu được
   // const linkBuilder = (table_uuid: string): string => {
-  //   const botUsername = "MiLiKun_bot"; 
-  //   const shortName = "orderfood"; 
-  //   const startParam = `tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
-  
-  //   return `tg://resolve?domain=${botUsername}&appname=${shortName}&startapp=${startParam}`;
+  //   return `https://zalo.me/s//menu/${store_uuid}/${table_uuid}?tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
   // };
+  // quét vào thẳng store nhưng không quét để sang menu được
+  const linkBuilder = (table_uuid: string): string => {
+    const botUsername = "MiLiKun_bot"; 
+    const shortName = "orderfood"; 
+    const startParam = `//menu/${store_uuid}/${table_uuid}?tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
+  
+    return `tg://resolve?domain=${botUsername}&appname=${shortName}&startapp=${startParam}`;
+  };
   
 
   const goToTableDetails = (tableUUID: string, tableName: string) => {
