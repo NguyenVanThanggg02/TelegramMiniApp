4cafa63cb962bd9bfe942a16684137dee499e34d
kun_dev <cong.nguyen@vangxanh.net>
2024-09-11 09:42:54 +0700

    up

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -264,7 +264,7 @@ const MenuCommonPage: React.FC<MenuCommonPageProps> = () => {
 
   const handleChangeTab = (value: string) => {
     const positionMenu = menu.map((m) => m.uuid).indexOf(value);
-    if (positionMenu === -1) return;
+    // if (positionMenu === -1) return;
     setActiveTab(value);
     if (!table_uuid) {
       setDefaultMarginList(40);
