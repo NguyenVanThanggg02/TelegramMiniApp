39306c13b9a3aed07e7748aae7ec2a5f94e701e7
kun_dev <cong.nguyen@vangxanh.net>
2024-09-12 15:51:13 +0700

    up

--- src/api/cloudStorageManager.ts
+++ src/api/cloudStorageManager.ts
@@ -8,25 +8,12 @@ let authTokenCache: string | undefined;
 let languageCache: string | undefined;
 
 const getSubdomain = async (): Promise<string | undefined> => {
-  try {
-    // Lấy giá trị subdomain mới nhất từ cloudStorage mỗi lần gọi hàm
+  if (subdomainCache === undefined) {
     const subdomain = await cloudStorage.get('subdomain');
-    return subdomain;
-  } catch (error) {
-    console.error("Error retrieving subdomain:", error);
-    return undefined;
+    subdomainCache = subdomain;
   }
+  return subdomainCache;
 };
-
-// Nếu bạn cần hàm cập nhật giá trị mới nhất vào cache, bạn có thể sử dụng hàm sau
-const refreshSubdomainCache = async (): Promise<void> => {
-  try {
-    subdomainCache = await cloudStorage.get('subdomain');
-  } catch (error) {
-    console.error("Error refreshing subdomain cache:", error);
-  }
-};
-
 
 const getAuthToken = async (): Promise<string | undefined> => {
   if (authTokenCache === undefined) {
@@ -46,4 +33,4 @@ const getLanguage = async (): Promise<string | undefined> => {
   return languageCache;
 };
 
-export { getSubdomain, getAuthToken, getLanguage,refreshSubdomainCache };
+export { getSubdomain, getAuthToken, getLanguage };
