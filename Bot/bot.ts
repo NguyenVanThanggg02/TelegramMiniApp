927b7f121fd4f724684f3ea4d07874c226a4cd7a
kun_dev <cong.nguyen@vangxanh.net>
2024-09-12 16:55:20 +0700

    up

--- Bot/bot.ts
+++ Bot/bot.ts
@@ -1,28 +1,28 @@
 import { Telegraf } from "telegraf";
+
 const TOKEN = "7273544566:AAFEYQS5oJZR0s9npHlbWwlBYcT1RKjoa3o";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://telegram-mini-app-psi.vercel.app/";
-bot.start((ctx) =>
-  ctx.reply("Welcome", {
-    reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-    },
-  }),
-);
-bot.on('text', (ctx) => {
-  const message = ctx.message.text;
-  if (message.startsWith('/start ')) {
-    const params = message.replace('/start ', ''); // Lấy tham số từ lệnh
 
+bot.start((ctx) => {
+  const message = ctx.message.text;
+  const params = message.replace('/start ', ''); // Lấy tham số từ lệnh
+
+  if (params) {
     const urlParams = new URLSearchParams(params);
     const tenantId = urlParams.get('tenant_id');
     const tableId = urlParams.get('tableId');
     const storeId = urlParams.get('storeId');
 
     ctx.reply(`Bot được khởi động với Tenant ID: ${tenantId}, Table ID: ${tableId}, Store ID: ${storeId}`);
+  } else {
+    ctx.reply("Welcome", {
+      reply_markup: {
+        keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+      },
+    });
   }
 });
-
 
 bot.launch();
