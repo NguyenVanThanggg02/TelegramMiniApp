b7b89d1213c1198d1408f7f983770e366d0166d6
kun_dev <cong.nguyen@vangxanh.net>
2024-09-12 16:49:45 +0700

    up

--- Bot/bot.ts
+++ Bot/bot.ts
@@ -1,5 +1,5 @@
 import { Telegraf } from "telegraf";
-const TOKEN = "";
+const TOKEN = "7273544566:AAFEYQS5oJZR0s9npHlbWwlBYcT1RKjoa3o";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://telegram-mini-app-psi.vercel.app/";
@@ -10,4 +10,19 @@ bot.start((ctx) =>
     },
   }),
 );
+bot.on('text', (ctx) => {
+  const message = ctx.message.text;
+  if (message.startsWith('/start ')) {
+    const params = message.replace('/start ', ''); // Lấy tham số từ lệnh
+
+    const urlParams = new URLSearchParams(params);
+    const tenantId = urlParams.get('tenant_id');
+    const tableId = urlParams.get('tableId');
+    const storeId = urlParams.get('storeId');
+
+    ctx.reply(`Bot được khởi động với Tenant ID: ${tenantId}, Table ID: ${tableId}, Store ID: ${storeId}`);
+  }
+});
+
+
 bot.launch();
