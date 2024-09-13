952614ac45fc19df03a49c88ebdd9a4ea205186b
kun_dev <cong.nguyen@vangxanh.net>
2024-09-13 08:10:39 +0700

    up

--- src/pages/admin/table/index.tsx
+++ src/pages/admin/table/index.tsx
@@ -97,12 +97,12 @@ const TablePage: React.FC = () => {
   // };
 
   const linkBuilder = (table_uuid: string): string => {
-    // const botUsername = "MiLiKun_bot"; 
+    const botUsername = "MiLiKun_bot"; 
     const shortName = "orderfood"; 
-    const startParam = '/menuu/${storeId}/${tableId}';
+    const startParam = '/menuu/${store_uuid}/${table_uuid}';
     console.log(table_uuid);
     
-    return `tg://resolve?domain=${shortName}&startapp=${startParam}`;
+    return `tg://resolve?domain=${botUsername}&appname=${shortName}&startapp=${startParam}`;
   };
   
 
