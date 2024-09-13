9ca168379395bb959905e7d4fa1577adc650d526
kun_dev <cong.nguyen@vangxanh.net>
2024-09-13 07:48:45 +0700

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -261,7 +261,7 @@ const Index: React.FC = () => {
       </Box>
       <Box flex flexDirection="column" className="section-container">
         <Box style={{ textAlign: "center" , color:'black'}}>
-          <Text size="xLarge" bold onClick={() =>redirectToMenu}>
+          <Text size="xLarge" bold onClick={() => navigate('/menuu/${storeId}/${tableId}')}>
             {t("main.scanQrDes")}
           </Text>
           {showScanner ? (
