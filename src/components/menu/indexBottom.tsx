e2dc278f3f8e4d55c211008312e1816fb3d0fb76
kun_dev <cong.nguyen@vangxanh.net>
2024-09-13 14:40:44 +0700

    up

--- src/components/menu/indexBottom.tsx
+++ src/components/menu/indexBottom.tsx
@@ -580,7 +580,7 @@ const MenuBottomCommonPage: React.FC<MenuCommonPageProps> = () => {
           borderTop: "1px solid #e0e0e0",
           backgroundColor: "#fff",
           position: "sticky",
-          bottom: 10, 
+          paddingBottom: "25px", 
           left: 0, 
           right: 0, 
         }}
