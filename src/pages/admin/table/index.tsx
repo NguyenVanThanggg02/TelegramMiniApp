f5e1ee137f9843a9d850e0de49af7af0e101d51d
kun_dev <cong.nguyen@vangxanh.net>
2024-09-14 16:29:00 +0700

    up

--- src/pages/admin/table/index.tsx
+++ src/pages/admin/table/index.tsx
@@ -92,17 +92,27 @@ const TablePage: React.FC = () => {
     }
   };
 
-  // const linkBuilder = (table_uuid: string): string => {
-  //   return `https://menu/${store_uuid}/${table_uuid}?tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
-  // };
+const linkBuilder = (table_uuid: string): string => {
+    return `https://menu/${store_uuid}/${table_uuid}?tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
+  };
 
-  const linkBuilder = (table_uuid: string): string => {
+  // const linkBuilder = (table_uuid: string): string => {
+  //   const botUsername = "MiLiKun_bot"; 
+  //   const shortName = "orderfood"; 
+  //   const startParam = `tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
+  //   return `tg://resolve?domain=${botUsername}&appname=${shortName}&startapp=${startParam}`;
+  // };
+  
+  const mobileLinkBuilder = (table_uuid: string): string => {
     const botUsername = "MiLiKun_bot"; 
     const shortName = "orderfood"; 
     const startParam = `tenant_id=${tenant_id}&tableId=${table_uuid}&storeId=${store_uuid}`;
+    
     return `tg://resolve?domain=${botUsername}&appname=${shortName}&startapp=${startParam}`;
   };
   
+  
+
 
   const goToTableDetails = (tableUUID: string, tableName: string) => {
     navigate({
@@ -182,11 +192,18 @@ const TablePage: React.FC = () => {
                 </Box>
                 <Box>
                   {selectedTableUUID === table.uuid && (
-                    <QRCodeViewer
-                      value={table.link}
-                      title={table.name.toUpperCase()}
-                      handleSave={handleSaveQr}
-                    />
+                    <>
+                      <QRCodeViewer
+                        value={table.link} 
+                        title={table.name.toUpperCase()}
+                        handleSave={handleSaveQr}
+                      />
+                      <QRCodeViewer
+                        value={mobileLinkBuilder(table.uuid)} 
+                        title={table.name.toUpperCase()}
+                        handleSave={handleSaveQr}
+                      />
+                    </>
                   )}
                 </Box>
               </Box>
@@ -198,8 +215,7 @@ const TablePage: React.FC = () => {
                 display: "flex",
                 justifyContent: "center",
                 flexDirection: "column",
-                marginTop:'50px'
-
+                marginTop: "50px",
               }}
             >
               <Text
