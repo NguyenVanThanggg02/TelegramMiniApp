8307b9cb1d58ebb54275372fe2b18aadd36fac98
kun_dev <cong.nguyen@vangxanh.net>
2024-09-18 08:44:26 +0700

    up

--- src/pages/admin/table/index.tsx
+++ src/pages/admin/table/index.tsx
@@ -117,8 +117,8 @@ const TablePage: React.FC = () => {
       element.current.style.fontFamily = "Montserrat";
       try {
         const dataUrl = await toPng(element.current, { cacheBust: true, backgroundColor: '#ffffff' });
-        // downloadImage(dataUrl, "qr-code.png");
-        downloadImage(dataUrl);
+        downloadImage(dataUrl, "qr-code.png");
+        // downloadImage(dataUrl);
         setSnackbarMessage(t("tableManagement.saveQrNoti"));
         setSnackbarType("success");
         setSnackbarOpen(true);
@@ -134,17 +134,18 @@ const TablePage: React.FC = () => {
     }
   };
   
-  // const downloadImage = (blob: string, fileName: string): void => {
-  //   const fakeLink = document.createElement("a");
-  //   fakeLink.style.display = "none";
-  //   fakeLink.download = fileName;
+  const downloadImage = (blob: string, fileName: string): void => {
+    const fakeLink = document.createElement("a");
+    fakeLink.setAttribute("sandbox", "allow-same-origin allow-scripts allow-downloads");
+    fakeLink.style.display = "none";
+    fakeLink.download = fileName;
 
-  //   fakeLink.href = blob;
-  //   document.body.appendChild(fakeLink);
-  //   fakeLink.click();
-  //   document.body.removeChild(fakeLink);
-  //   fakeLink.remove();
-  // };
+    fakeLink.href = blob;
+    document.body.appendChild(fakeLink);
+    fakeLink.click();
+    document.body.removeChild(fakeLink);
+    fakeLink.remove();
+  };
 
   // k báo k cho phép down nhưng hiển thị thẻ iframe ở ui
 //   const downloadImage = (blob: string): void => {
@@ -169,22 +170,22 @@ const TablePage: React.FC = () => {
 //   };
 // };
 
-const downloadImage = (blobUrl: string): void => {
-  const iframe = document.createElement("iframe");
-  iframe.setAttribute("sandbox", "allow-same-origin allow-scripts allow-downloads"); 
-  iframe.style.display = "none";
-  document.body.appendChild(iframe);
+// const downloadImage = (blobUrl: string): void => {
+//   const iframe = document.createElement("iframe");
+//   iframe.setAttribute("sandbox", "allow-same-origin allow-scripts allow-downloads"); 
+//   iframe.style.display = "none";
+//   document.body.appendChild(iframe);
 
-  iframe.onload = () => {
-    const downloadLink = document.createElement("a");
-    downloadLink.href = blobUrl; 
-    downloadLink.download = "qrcode.png"; 
-    downloadLink.click(); 
-    document.body.removeChild(iframe); 
-  };
+//   iframe.onload = () => {
+//     const downloadLink = document.createElement("a");
+//     downloadLink.href = blobUrl; 
+//     downloadLink.download = "qrcode.png"; 
+//     downloadLink.click(); 
+//     document.body.removeChild(iframe); 
+//   };
 
-  iframe.src = blobUrl; 
-};
+//   iframe.src = blobUrl; 
+// };
 
   return (
     <Page className="page">
