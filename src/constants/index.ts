d14b51a267cf547303cca4eb37490bee32467ad4
kun_dev <cong.nguyen@vangxanh.net>
2024-09-20 16:30:08 +0700

    up

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,6 +6,7 @@ export const DEFAULT_TENANT_ID = import.meta.env.VITE_DEFAULT_TENANT_ID;
 export const BOT_USERNAME =import.meta.env.VITE_BOT_USERNAME
 export const SHORT_NAME =import.meta.env.VITE_SHORT_NAME
 export const BOT =import.meta.env.VITE_BOT
+export const CHAT_ID =import.meta.env.VITE_CHAT_ID
 export const APP_VERSION = appConfig.app.version;
 export const DEFAULT_PER_PAGE = 100;
 
