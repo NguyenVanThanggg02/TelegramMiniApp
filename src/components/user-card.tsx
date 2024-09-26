f6dca65d779a08a9b74ac360207c07c9d305cf7c
kun_dev <cong.nguyen@vangxanh.net>
2024-09-26 10:26:53 +0700

    update avatar

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -52,7 +52,7 @@ const UserCard: React.FC<UserCardProps> = ({ isAdmin = false, showOrderHistory =
             }
           >
             {!user.avatar || !user.avatar.startsWith("http") ? (
-              <span style={{ fontSize: "25px", fontWeight: "bold" }}>
+              <span style={{ fontSize: "15px", fontWeight: "bold" }}>
                 {avt.join("")}
               </span>
             ) : null}
