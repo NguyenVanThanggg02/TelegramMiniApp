bd1860c0e704ac538a9645c5ff7e40b89079cc19
kun_dev <cong.nguyen@vangxanh.net>
2024-09-26 10:22:56 +0700

    update avatar

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -3,6 +3,7 @@ import { Avatar, Box, Text } from 'zmp-ui';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../state';
 import { useNavigate } from 'react-router-dom';
+import { useInitData } from '@telegram-apps/sdk-react';
 
 interface UserCardProps {
   isAdmin?: boolean;
@@ -13,7 +14,11 @@ const UserCard: React.FC<UserCardProps> = ({ isAdmin = false, showOrderHistory =
   const navigate = useNavigate();
   const user = useRecoilValue(userState);
   const [userLoaded, setUserLoaded] = useState(false);
-
+  const initData = useInitData();
+  
+  const avt = [(initData?.user?.firstName?.charAt(0)), (initData?.user?.lastName?.charAt(0))]
+  console.log(avt.toString().split(",")); 
+  console.log(avt.join("")); 
   const goToUserProfile = () => {
     if (isAdmin) {
       const params = new URLSearchParams();
@@ -38,13 +43,22 @@ const UserCard: React.FC<UserCardProps> = ({ isAdmin = false, showOrderHistory =
         <>
           <Avatar
             story="default"
-            online
-            src={user.avatar.startsWith('http') ? user.avatar : undefined}
             onClick={goToUserProfile}
+            online={true}
+            src={
+              user.avatar && user.avatar.startsWith("http")
+                ? user.avatar
+                : undefined
+            }
           >
-            {user.avatar}
+            {!user.avatar || !user.avatar.startsWith("http") ? (
+              <span style={{ fontSize: "40px", fontWeight: "bold" }}>
+                {avt.join("")}
+              </span>
+            ) : null}
           </Avatar>
-          <Box ml={4} style={{color:'black'}}>
+
+          <Box ml={4} style={{ color: "black" }}>
             <Text.Title>{user.name}</Text.Title>
             <Text>{user.role}</Text>
           </Box>
