3afb31ecab3a2e93295cdee4bc29b88a155347d6
kun_dev <cong.nguyen@vangxanh.net>
2024-09-28 08:36:53 +0700

    update currency

--- src/pages/admin/store/index.tsx
+++ src/pages/admin/store/index.tsx
@@ -3,7 +3,6 @@ import {
   Box,
   Page,
   Select,
-  Spinner,
   Button,
 } from "zmp-ui";
 import { useRecoilState } from "recoil";
@@ -76,7 +75,6 @@ const StorePage: React.FC = () => {
   const [snackbarType, setSnackbarType] = useState<"success" | "error">("success");
   
   const [store, setStore] = useRecoilState(storeState);
-  const [folowOALoading, ] = useState<boolean>(false);
   const cloudStorage = initCloudStorage();
 
   useEffect(() => {
@@ -339,9 +337,12 @@ const StorePage: React.FC = () => {
             onClick={handleJoinChannel}
             className="store_list-item_main_item"
           >
-              <div className="spinner-container">
-                <Spinner visible={folowOALoading} logo={appLogo} />
-              </div>
+            <img className="icon-img" src={appLogo} alt="App Logo" />
+            <span style={{ color: "black" }}>
+              {user?.is_oa_follow
+                ? t("storeManagement.send_message")
+                : t("storeManagement.follow_oa")}
+            </span>
           </Box>
         </Box>
       </Box>
