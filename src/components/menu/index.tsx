d4944158f59bd3f4b8bf475aca4898a6b2d1b5af
kun_dev <cong.nguyen@vangxanh.net>
2024-10-01 13:09:32 +0700

    tab

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -261,44 +261,35 @@ const MenuCommonPage: React.FC<MenuCommonPageProps> = () => {
     );
     setDisplayProductList(result);
   }, [productList, categoryList]);
-
-  // const handleChangeTab = (value: string) => {
-  //   const positionMenu = menu.map((m) => m.uuid).indexOf(value);
-  //   if (positionMenu === -1) return;
-  //   setActiveTab(value);
-  //   if (!table_uuid) {
-  //     setDefaultMarginList(40);
-  //   }
-  //   menuRef.current[positionMenu]?.scrollIntoView({
-  //     behavior: "smooth",
-  //     block: "start",
-  //   });
-  // };
+  
   const handleChangeTab = (value: string) => {
     const positionMenu = menu.map((m) => m.uuid).indexOf(value);
-    if (positionMenu === -1) return;
+    if (positionMenu === -1 || !pageRef.current) return; 
+  
     setActiveTab(value);
-    if (positionMenu === menu.length - 1) {
-      const secondLastTabPosition = menu.length - 1;
-      const footerElement = document.querySelector(".web-app-footer");
-      if (footerElement) {
-        footerElement.remove();
+    const selectedMenuRef = menuRef.current[positionMenu];
+  
+    if (selectedMenuRef) {
+      const menuHeight = selectedMenuRef.offsetHeight; 
+      const scrollPosition = selectedMenuRef.getBoundingClientRect().top + (pageRef.current.scrollTop || 0); 
+  
+      if (table_uuid) {
+        setDefaultMarginList(40); 
       }
-      menuRef.current[secondLastTabPosition]?.scrollIntoView({
-        behavior: "smooth",
-        // block: "start",
-      });
-      return;
+  
+      if (positionMenu === menu.length - 1) {
+        pageRef.current.scrollTo({
+          top: scrollPosition + menuHeight, 
+          behavior: 'smooth',
+        });
+      }else{
+        menuRef.current[positionMenu]?.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
     }
-    if (!table_uuid) {
-      setDefaultMarginList(40);
-    }
-    menuRef.current[positionMenu]?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
   };
-
   const fetchCategoriesByStore = async (store_uuid: string) => {
     const response = await getCategoryByStore(store_uuid);
     
