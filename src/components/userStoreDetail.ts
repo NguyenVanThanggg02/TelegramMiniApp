e53a91ecad48c108f744b1a143ad0be81fc54f55
kun_dev <cong.nguyen@vangxanh.net>
2024-10-03 13:22:38 +0700

    update currency

--- src/components/userStoreDetail.ts
+++ src/components/userStoreDetail.ts
@@ -4,20 +4,18 @@ import { getStoreByUUID } from '@/api/api';
 import { useRecoilState } from 'recoil';
 import { loadingState } from '@/state';
 
-
 const useStoreDetail = () => {
   const { store_uuid } = useParams<{ store_uuid?: string }>();
-  // const [currency, setCurrency] = useState('$');
-  const [currency, setCurrency] = useState<string | null>(null);
+  const [currency, setCurrency] = useState('$');
   const [loading, setLoading] = useRecoilState(loadingState);
 
   const getStoreDetail = async () => {
+    setLoading({ ...loading, isLoading: true }); 
     if (store_uuid) {
       const response = await getStoreByUUID(store_uuid);
       if (response.data) {
         const metadata = JSON.parse(response.data.metadata);
         const currencyValue = metadata.currency || '$'; 
-        console.log(currencyValue);
         setCurrency(currencyValue);
       } else {
         console.error("Error fetching store data:", response.error);
@@ -28,7 +26,7 @@ const useStoreDetail = () => {
 
   useEffect(() => {
     getStoreDetail();
-  }, [store_uuid]);
+  }, []);
 
   return { currency, loading };
 };
