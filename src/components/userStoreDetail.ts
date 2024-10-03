241ba34cf98ad48f33f897c8e30a457d8381632c
kun_dev <cong.nguyen@vangxanh.net>
2024-10-03 09:34:28 +0700

    update loading

--- src/components/userStoreDetail.ts
+++ src/components/userStoreDetail.ts
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getStoreByUUID } from '@/api/api';
-// import { useRecoilState } from 'recoil';
-// import { loadingState } from '@/state';
+import { useRecoilState } from 'recoil';
+import { loadingState } from '@/state';
 
 const useStoreDetail = () => {
   const { store_uuid } = useParams<{ store_uuid?: string }>();
-  const [currency, setCurrency] = useState<string | null>(null); 
-  // const [loading, setLoading] = useRecoilState(loadingState);
+  const [currency, setCurrency] = useState('$');
+  const [loading, setLoading] = useRecoilState(loadingState);
 
   const getStoreDetail = async () => {
+    setLoading({ ...loading, isLoading: true }); 
     if (store_uuid) {
       const response = await getStoreByUUID(store_uuid);
       if (response.data) {
@@ -19,16 +20,15 @@ const useStoreDetail = () => {
       } else {
         console.error("Error fetching store data:", response.error);
       }
-      // setLoading({ ...loading, isLoading: false }); 
+      setLoading({ ...loading, isLoading: false }); 
     }
   };
 
   useEffect(() => {
-    // setLoading({ ...loading, isLoading: true }); 
     getStoreDetail();
   }, []);
 
-  return { currency };
+  return { currency, loading };
 };
 
 export default useStoreDetail;
