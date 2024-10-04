0cb9a8e32cc9a9006cbe7e4bb135d18862c24429
congnv <conganh28999@example.com>
2024-10-04 21:20:06 +0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import { Root } from '@/components/Root';
+import { isTMA } from '@telegram-apps/sdk';
 
 // Uncomment this import in case, you would like to develop the application even outside
 // the Telegram application, just in your browser.
@@ -10,3 +11,15 @@ import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
+
+isTMA()
+  .then((result) => {
+    if (result) {
+      console.log("Đang chạy trong Telegram Mini App.");
+    } else {
+      console.log("Không chạy trong Telegram Mini App.");
+    }
+  })
+  .catch((error) => {
+    console.error("Lỗi khi kiểm tra môi trường Telegram Mini App:", error);
+  });
