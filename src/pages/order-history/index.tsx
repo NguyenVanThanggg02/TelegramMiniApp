ec6a42d1fac9687a7d75667a1f5b2905f033a2b0
kun_dev <cong.nguyen@vangxanh.net>
2024-10-11 14:18:25 +0700

    u

--- src/pages/order-history/index.tsx
+++ src/pages/order-history/index.tsx
@@ -101,7 +101,7 @@ const OrderHistory: React.FC = () => {
 
   const getHistoryOrders = async () => {
     const data = await fetchHistoryOrdersByStore();
-    console.log(data.data.store_uuid[0]);
+    console.log(data.data);
     
     if (!data?.error) {
       const orders = data.data as Order[]; 
