10d470a594834b7ee9e247c98d58e4aa2060a519
kun_dev <cong.nguyen@vangxanh.net>
2024-10-17 14:01:21 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+declare global {
+    interface Window {
+      Telegram?: any;
+    }
+  }
+  
 import ReactDOM from 'react-dom/client';
 
 import { Root } from '@/components/Root';
@@ -9,4 +15,15 @@ import './mockEnv.ts';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
+// ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
+
+
+const isRunningInTelegram = window.Telegram !== undefined;
+
+if (isRunningInTelegram) {
+  // Chạy trong Telegram, render vào 'root'
+  ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
+} else {
+  // Chạy ngoài trình duyệt thông thường, render vào 'root1'
+  ReactDOM.createRoot(document.getElementById('root1')!).render(<Root />);
+}
