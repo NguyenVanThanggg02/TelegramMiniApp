b3284a882db29e1e32272676ae3baac620348ee7
kun_dev <cong.nguyen@vangxanh.net>
2024-10-17 14:42:58 +0700

    up detect

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,4 @@ import './mockEnv.ts';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
-import { useInitData } from '@telegram-apps/sdk-react';
-
-const initData = useInitData();
-console.log(initData);
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
-
-if (initData) {
-    ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
-  } else {
-    ReactDOM.createRoot(document.getElementById('root1')!).render(<Root />);
-  }
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
