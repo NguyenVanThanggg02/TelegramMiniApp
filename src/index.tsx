08c70e33eb3b5a1321e637741ecd0d5d8732ac77
kun_dev <cong.nguyen@vangxanh.net>
2024-10-18 14:34:10 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import { useEffect } from 'react';
-import { Root } from '@/components/Root';
-import { useLaunchParams } from '@telegram-apps/sdk-react'; // Sử dụng launch params từ SDK
 
+import { Root } from '@/components/Root';
+
+// Uncomment this import in case, you would like to develop the application even outside
+// the Telegram application, just in your browser.
 import './mockEnv.ts';
+
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
-const App = () => {
-  const launchParams = useLaunchParams();
-
-  useEffect(() => {
-    const rootId = launchParams ? 'root' : 'root1';
-    const rootElement = document.getElementById(rootId);
-
-    if (rootElement) {
-      ReactDOM.createRoot(rootElement).render(<Root />);
-    } else {
-      console.error(`Không tìm thấy phần tử có id là ${rootId}`);
-    }
-  }, [launchParams]);
-
-  return null;
-};
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root1')!).render(<Root/>);
