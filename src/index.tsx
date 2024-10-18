330aa59beb41e3aee68df0f83a131186ca9931b2
kun_dev <cong.nguyen@vangxanh.net>
2024-10-18 10:03:01 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { useEffect, useState } from 'react';
+
 import { Root } from '@/components/Root';
 
 // Uncomment this import in case, you would like to develop the application even outside
@@ -9,32 +9,4 @@ import './mockEnv.ts';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
-import { useInitData } from '@telegram-apps/sdk-react';
-
-const AppLoader = () => {
-  const [rootId, setRootId] = useState<string | null>(null);
-  const initData = useInitData();
-
-  useEffect(() => {
-    if (initData) {
-      setRootId('root'); // Nếu có initData, render vào root
-    } else {
-      setRootId('root1'); // Nếu không có initData, render vào root1
-    }
-  }, [initData]);
-
-  useEffect(() => {
-    if (rootId) {
-      const rootElement = document.getElementById(rootId);
-      if (rootElement) {
-        ReactDOM.createRoot(rootElement).render(<Root />);
-      } else {
-        console.error(`Không tìm thấy element có id là ${rootId}`);
-      }
-    }
-  }, [rootId]);
-
-  return null;
-};
-
-AppLoader();
+ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
